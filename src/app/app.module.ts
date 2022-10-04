36a21aa82b151e650b6a903f5c46b5c78693185f
DMWebDev <89517604+SuperDMWebDev@users.noreply.github.com>
2022-10-04 23:32:41 +0700

    messages + service  + heroes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,8 +4,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
 import { HeroesComponent } from './heroes/heroes.component';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+import { MessagesComponent } from './messages/messages.component';
 @NgModule({
-  declarations: [AppComponent, HeroesComponent],
+  declarations: [AppComponent, HeroesComponent, HeroDetailComponent, MessagesComponent],
   imports: [BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
-import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service';
+import { MessageService } from '../message.service';
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
@@ -11,11 +12,20 @@ export class HeroesComponent implements OnInit {
     id: 1,
     name: 'Winstorm',
   };
-  heroes = HEROES;
+  heroes: Hero[] = [];
   selectedHero?: Hero = null;
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
+    this.messageService.add(`Hero id add ${hero.id}`);
   }
-  constructor() {}
-  ngOnInit(): void {}
+  getHeroes(): void {
+    this.heroService.getHeroes().subscribe((heroes) => (this.heroes = heroes));
+  }
+  constructor(
+    private heroService: HeroService,
+    public messageService: MessageService
+  ) {}
+  ngOnInit(): void {
+    this.getHeroes();
+  }
 }
